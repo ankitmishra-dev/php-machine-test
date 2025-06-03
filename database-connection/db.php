a22a47307f22abadf89c07954e0c46791f2f4f4b
Ankit Mishra <ankitmishra8268@gmail.com>
2025-06-03 22:15:11 +0530

    update creds using constant

--- database-connection/db.php
+++ database-connection/db.php
@@ -1,11 +1,13 @@
 <?php
-$host = '127.0.0.1';
-$db   = 'student_test';
-$user = 'root';
-$pass = ''; // Set your DB password
+
+define('DB_HOST', '127.0.0.1');
+define('DB_NAME', 'students_crud');
+define('DB_USER', 'root');
+define('DB_PASS', '');
 
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass);
+    $connectionString = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME;
+    $conn = new PDO($connectionString, DB_USER, DB_PASS);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
